4f64b2c35176c69b2e7bbdb7cc8a8439a92290bb
Eric Yao <ericyaohan@yahoo.com>
2025-02-19 23:46:47 -0500

    created message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -13,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("server is running on port " + PORT);
