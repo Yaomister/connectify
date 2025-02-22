48049235c26472e8ff6174891e6ddc419cfbc583
Eric Yao <ericyaohan@yahoo.com>
2025-02-21 23:16:56 -0500

    created frontend routes

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { protectRoute } from "../middleware/auth.middleware";
+import { protectRoute } from "../middleware/auth.middleware.js";
 import {
   getMessages,
   getUsersForSidebar,
-} from "../controllers/message.controller";
+  sendMessage,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
